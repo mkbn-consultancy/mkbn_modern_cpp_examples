42c1a183fe8c9951a1c3d863e396130e53fb142d
mkbn-consultancy <79524974+mkbn-consultancy@users.noreply.github.com>
2023-05-29 17:10:19 +0300

    Update 4_count_div_by_b_exercise.cpp

--- metaprogramming/4_count_div_by_b_exercise.cpp
+++ metaprogramming/4_count_div_by_b_exercise.cpp
@@ -4,19 +4,9 @@
 
 //TODO: Implement count_div_by_b_numbers_till_a
 
-template<bool B>
-struct IsDivBy{
-    enum{result = B};
-};
-
-template<unsigned int a, unsigned int b>
-struct count_div_by_b_numbers_till_a{
-    enum{value = IsDivBy<(a%b==0)>::result + count_div_by_b_numbers_till_a<a-1,b>::value};
-};
-
 
 int main()
 {
     std::cout<<"#numbers till 20 divided by 3 is: "<<
-    count_div_by_b_numbers_till_a<20,0>::value<<std::endl;
+                count_div_by_b_numbers_till_a<20,0>::value<<std::endl;
 }

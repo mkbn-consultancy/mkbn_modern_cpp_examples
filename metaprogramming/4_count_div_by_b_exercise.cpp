ff9cff6d61aba7bcf428b74846a8e5f928ec1f0d
mkbn-consultancy <79524974+mkbn-consultancy@users.noreply.github.com>
2023-05-30 10:16:25 +0300

    Update 4_count_div_by_b_exercise.cpp

--- metaprogramming/4_count_div_by_b_exercise.cpp
+++ metaprogramming/4_count_div_by_b_exercise.cpp
@@ -8,5 +8,5 @@
 int main()
 {
     std::cout<<"#numbers till 20 divided by 3 is: "<<
-                count_div_by_b_numbers_till_a<20,0>::value<<std::endl;
+                count_div_by_b_numbers_till_a<20,3>::value<<std::endl;
 }
